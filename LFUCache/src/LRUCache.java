98a280714b08883ed553afaa17aed062d37970cd
Aman Jham <ajham6222@gmail.com>
2023-02-16 19:01:20 +0530

    Create LRUCache.java
    Added LRU Cache implementation, uses same artifacts as LFU Cache

---
+++ LFUCache/src/LRUCache.java
@@ -0,0 +1,37 @@
+class LRUCache {
+    private int capacity;
+    private HashMap<Integer, Node> keyToNode;
+    private DLL list;
+
+    LRUCache(int capacity) {
+        this.capacity = capacity;
+        keyToNode = new HashMap<>();
+        list = new DLL();
+    }
+    
+    int get(int key) {
+        if (!keyToNode.containsKey(key)) {
+            return -1;
+        }
+        Node node = keyToNode.get(key);
+        list.remove(node);
+        list.add(node);
+        return node.getValue();
+    }
+    
+    void put(int key, int value) {
+        if (keyToNode.containsKey(key)) {
+            Node node = keyToNode.get(key);
+            node.setValue(value);
+            get(key);
+            return;
+        }
+        if (keyToNode.size() == capacity) {
+            int keyRemoved = list.remove();
+            keyToNode.remove(keyRemoved);
+        }
+        Node node = new Node(key, value);
+        keyToNode.put(key, node);
+        list.add(node);
+    }
+};
